6fe913d81a6da9fffce36e1de7574496dd43c1e8
蛋炒饭要加葱花 <7718198+Rickgy@user.noreply.gitee.com>
2021-06-12 19:55:52 +0800

    项目初始化

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+'use strict'
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: '@/components',
+        api: '@/api',
+        utils: '@/utils',
+        store: '@/store',
+        router: '@/router',
+        views: '@/views'
+      }
+    }
+  }
+}
